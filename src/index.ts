50498ca52684da3b673121bad4c9ab1f58075923
Basabe-Gabriel <92063962+Basabe-Gabriel@users.noreply.github.com>
2021-11-07 09:23:34 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+// esto es una suma
+//let n1: number = 5;
+//let n2: number = 5;
+//let suma: number,
+//suma = n1 + n2;
+
+//console.log("la suma es: " + suma);
+//---------------------------------------
+// ahroa vamos con una resta
+
+//let n1: number =5;
+//let n2: number =5;
+//let resta : number;
+
+//let resta = n1-n2;
+
+//console.log("el resultado es: "+resta);
+//------------------------------------------
+
+// ahora vamos por una divicion
+
+//let n1 : number =4;
+//let n2 : number =2;
+//let div = n1 / n2;
+
+//console.log("el resultado es: "+div);
+
+//------------------------------------------
+
+//ahora vamos con una multiplacion
+
+let n1: number = 5;
+let n2: number = 5;
+let multi: number;
+
+let multi = n1 * n2;
+
+console.log("el resultado es: " + multi);
